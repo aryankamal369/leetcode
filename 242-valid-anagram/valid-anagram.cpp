5488a2b7119ec13b40119ac634037f155f170f9c
Aryan Kamal <113275639+aryankamal369@users.noreply.github.com>
2026-08-10 16:31:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (49.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        unordered_map<char, int> mp;
-        for(char ch : s){
-            mp[ch]++;
+
+        // if(s.size()!=t.size()) return false;
+
+        // unordered_map<char, int> mp;
+        // for(char ch : s){
+        //     mp[ch]++;
+        // }
+
+        // for(char ch : t){
+        //     mp[ch]--;
+        // }
+
+        // for(auto it : mp){
+        //     if(it.second>0){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+        if(s.size() != t.size()) return false;
+        vector<int>ans(26, 0);
+        for(int i=0; i<s.size(); i++){
+            ans[s[i]-'a']++;
         }
 
-        for(char ch : t){
-            mp[ch]--;
+        for(int i=0; i<t.size(); i++){
+            ans[t[i]-'a']--;
         }
 
-        for(auto it : mp){
-            if(it.second>0){
+        for(int i=0; i<ans.size(); i++){
+            if(ans[i]>0){
                 return false;
             }
         }
