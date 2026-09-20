8e393e44c948a68711fcb7662d5fc2fe4e57d1e8
Aryan Kamal <113275639+aryankamal369@users.noreply.github.com>
2026-09-20 09:23:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.size();
         int ans = 0;
         for(int i=0; i<n; i++){
-            ans += (27- (s[i]-'a'+1))*(i+1);
+            ans += (26-s[i]+'a')*(i+1);
         }
         return ans;
     }
